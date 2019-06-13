548fb72b4deec9cf7d8a16bc7decdeb7f06e6762
haigol <hkgolber@stud.ntnu.no>
2019-06-13 15:18:15 +0200

    separated files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import Board from './Board';
+// import Square from './Square';
+import Game from './Game';
 
+ReactDOM.render(<Game />,
+    document.getElementById('root')
+  );
